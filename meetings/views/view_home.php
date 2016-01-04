97eb60768f9ca376ec19a16d4f45f8abd71993ce
Vitnere <nemanjakolar@gmail.com>
2016-01-04 19:54:51 +0100

    navbar dev

---
+++ meetings/views/view_home.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+
+	
+<head>
+	<meta charset="utf-8">
+	<title>Talk 2 Bit</title>
+
+	<!--page-->
+<div id="container">
+	<h1>Welcome to Vitnere web sajt</h1>
+
+	<!--first nav-->
+<div id="container"
+	<nav class="navbar navbar-default navbar-fixed-top">
+  		<div class="container">
+	     <ul>
+	     	<li>Home</li>
+	     	<li>How it works</li>
+	     	<li>Bilateral Meeting-How it works</li>
+	     	<li>Programme</li>
+	     	<li>FAQ</li>
+	     	<li>Contact</li>
+	     </ul>
+ 	 	</div>
+	</nav>
+</div>
+
+	<!--body-->
+	<div id="body">
+		
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+</div>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
