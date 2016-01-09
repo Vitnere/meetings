48af921d097d061347f3ad82ec6ef3f0fe27143c
Vitnere <nemanjakolar@gmail.com>
2016-01-09 18:58:29 +0100

    building and styling main

--- app/views/main.php
+++ app/views/main.php
@@ -118,37 +118,49 @@
 
     <!--main-->
     <div class="col-lg-12" id="main">
-        <div class="col-lg-8">
-            <h4>GO INTERNATIONAL - ICT BROKERAGE EVENT 2015</h4>   
-            <br />
-            <p>Get connected and take advantage of ICT networking 
-            opportunities by discovering new international partnerships 
-            to grow your business.</p>  
-            <br />
-            <p>Enterprise Europe Network in Macedonia is pleased 
-            to invite you to participate on business meetings that will 
-            take place within Go International -<span id="blue_text"> ICT Brokerage Event 2015 
-            on November 13 in Skopje in Hotel Continental, Skopje.</span></p>
-            <br />
-            <p>The event is free of charge and targets companies and organizations
-             working in the ICT sector interested in presenting and discovering
-              new products and services, exploring new business opportunities, 
-              finding new potential clients and partners for creating new business 
-              opportunities and industry partnerships for reaching international
-               markets.</p>
-            <br />
-            <p>The Go International - ICT Brokerage Event 2015 is organized by the
-             Foundation for Management and Industrial Research, Youth Entrepreneurial 
-             Service Foundation and cooperating network partners: University of Novi Sad 
-             - Serbia, ARC Consulting – Bulgaria and Chamber of Economy - Montenegro.</p>
-            <br />
-            <p>The event is supported by the Ministry of Economy of the Republic 
-            of Macedonia and CEI – Central European Initiative.</p>  
-        </div>  
-        <div class="col-lg-4">
-            
-        </div>    
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                <h1>GO INTERNATIONAL - ICT <br />BROKERAGE EVENT 2015</h1>   
+                <br />
+                <p>Get connected and take advantage of ICT networking 
+                opportunities by discovering new international partnerships 
+                to grow your business.</p>  
+                <br />
+                <p>Enterprise Europe Network in Macedonia is pleased 
+                to invite you to participate on business meetings that will 
+                take place within Go International -<span id="blue_text"> ICT Brokerage Event 2015 
+                on November 13 in Skopje in Hotel Continental, Skopje.</span></p>
+                <br />
+                <p>The event is free of charge and targets companies and organizations
+                 working in the ICT sector interested in presenting and discovering
+                  new products and services, exploring new business opportunities, 
+                  finding new potential clients and partners for creating new business 
+                  opportunities and industry partnerships for reaching international
+                   markets.</p>
+                <br />
+                <p>The Go International - ICT Brokerage Event 2015 is organized by the
+                 Foundation for Management and Industrial Research, Youth Entrepreneurial 
+                 Service Foundation and cooperating network partners: University of Novi Sad 
+                 - Serbia, ARC Consulting – Bulgaria and Chamber of Economy - Montenegro.</p>
+                <br />
+                <p>The event is supported by the Ministry of Economy of the Republic 
+                of Macedonia and CEI – Central European Initiative.</p>  
+                </div>  
+                <div class="col-md-4" id="main_right">
+                    <h3>BILATERAL TALKS</h3>
+                    <span>PARTICIPANTS</span><br />
+                    <span>MEETINGS REQUESTED</span><br />
+                    <span>MEETINGS ACCEPTED</span><br />
+                   
+                    <span>89</span><br />   
+                   <span>339</span><br />
+                    <span>165</span><br />
+                </div>    
+            </div>
+        </div>
     </div>
+    
     <!--/.main-->
 
     <!-- Footer -->
