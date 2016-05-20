9bc6199ef2ccb678802bb012aaa7a15e35c095ae
Vitnere <nemanjakolar@gmail.com>
2016-05-20 10:56:43 +0200

    admin gallery build 3.1

--- app/views/gallery/guest.php
+++ app/views/gallery/guest.php
@@ -16,67 +16,67 @@
         <ul class="participants">
 
             <li>
-                <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Bosnia_Herzegovina.png')?> "/> </span>
+
                 <span><a href="#">Bosnia-Herzegovina</a></span>
                 <span class="number">7</span>
             </li>
 
             <li>
-                <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Bulgaria.png')?> "/> </span>
+
                 <span><a href="#">Bulgaria</a></span>
                 <span class="number">4</span>
             </li>
 
             <li>
-                <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Greece.png')?> "/></span>
+
                 <span><a href="#">Greece</a></span>
                 <span class="number">3</span>
             </li>
 
             <li>
-                <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Hungary.png')?> "/></span>
+
                 <span><a href="/members/country/5/Hungary">Hungary</a></span>
                 <span class="number">1</span>
             </li>
 
             <li>
-                <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Italy.png')?> "/></span>
+
                 <span><a href="/members/country/6/Italy">Italy</a></span>
                 <span class="number">1</span>
             </li>
 
             <li>
-                    <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Macedonia.png" alt="Macedonia.png')?> /></span>
+
                     <span><a href="/members/country/126/Macedonia">Macedonia</a></span>
                 <span class="number">47</span>
             </li>
 
             <li>
-                    <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Montenegro.png" alt="Montenegro.png')?> /></span>
+
                     <span><a href="/members/country/140/Montenegro">Montenegro</a></span>
                 <span class="number">7</span>
             </li>
 
             <li>
-                    <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Poland.png" alt="Poland.png')?> />
+
                     </span><span><a href="/members/country/168/Poland">Poland</a></span>
                 <span class="number">3</span>
             </li>
 
             <li>
-                    <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Romania.png" alt="Romania.png')?> /></span>
+
                     <span><a href="/members/country/11/Romania">Romania</a></span>
                 <span class="number">3</span>
             </li>
 
             <li>
-                    <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Sr.png" alt="Sr.png')?> /></span>
+
                     <span><a href="/members/country/7/Serbia">Serbia</a></span>
                 <span class="number">9</span>
             </li>
 
             <li>
-                    <span class="flag"><img src="<?=base_url('assets/img/thumbs/24z24_Slovenia.png" alt="Slovenia.png')?> />
+
                     </span><span><a href="/members/country/185/Slovenia">Slovenia</a></span>
                 <span class="number">4</span>
             </li><br>
