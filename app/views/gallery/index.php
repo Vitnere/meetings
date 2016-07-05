ca41e756450f76d257bc003b8f6680ca5d914a2a
Vitnere <nemanjakolar@gmail.com>
2016-07-05 13:35:01 +0200

    AD Build 3.5 FE gallery update

--- app/views/gallery/index.php
+++ app/views/gallery/index.php
@@ -26,35 +26,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
             <div class="col-md-12" align="center">
                 <a class="btn btn-primary" href="<?php echo base_url()?>">Index</a>
-                <?=anchor('gallery/add','Add a new image',['class'=>'btn btn-primary'])?>
-                <br /><br />
-            </div>
+                <?=anchor('gallery/add','Add photo',['class'=>'btn btn-primary'])?>
+                <!-- Large modal -->
+                <button class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Gallery</button>
+                <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg col-md-8">
+                        <div class="modal-content">
 
-
-            <hr />
-
-
-            <div class="col-md-12">
-                <?php foreach($images->result() as $img) : ?>
-                    <div class="col-md-4">
-                        <div class="thumbnail">
-                            <?=img($img->file)?>
-                            <div class="caption">
-                                <h3><?=$img->caption?></h3>
-                                <p><?=substr($img->description, 0,100)?>...</p>
-                                <p>
-                                    <?=anchor('gallery/edit/'.$img->id,'Edit',['class'=>'btn btn-warning', 'role'=>'button'])?>
-                                    <?=anchor('gallery/delete/'.$img->id,'Delete',['class'=>'btn btn-danger', 'role'=>'button','onclick'=>'return confirm(\'Are you sure?\')'])?>
-                                </p>
+                            <!-- Gallery-->
+                            <div class="col-md-12">
+                                <?php foreach($images->result() as $img) : ?>
+                                    <div class="col-md-4">
+                                        <div class="thumbnail">
+                                            <?=img($img->file)?>
+                                            <div class="caption">
+                                                <h3><?=$img->caption?></h3>
+                                                <p><?=substr($img->description, 0,100)?>...</p>
+                                                <p>
+                                                    <?=anchor('gallery/edit/'.$img->id,'Edit',['class'=>'btn btn-warning', 'role'=>'button'])?>
+                                                    <?=anchor('gallery/delete/'.$img->id,'Delete',['class'=>'btn btn-danger', 'role'=>'button','onclick'=>'return confirm(\'Are you sure?\')'])?>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
                             </div>
+                            <!-- Gallery end-->
                         </div>
                     </div>
-                <?php endforeach; ?>
+                </div>
             </div>
-
-
-
-
+            <hr />
         <?php else : ?>
             <div align="center">We don't have any image yet, go ahead and <?=anchor('gallery/add','add a new one')?>.</div>
         <?php endif; ?>
